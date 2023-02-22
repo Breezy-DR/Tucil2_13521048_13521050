4b27899d6d2d5781cc9e2a84dbfef059f001ac9b
BreezyDR <13521048@std.stei.itb.ac.id>
2023-02-22 20:42:58 +0700

    Create dot.py

---
+++ src/dot.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from math import sqrt
+
+class Dot:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        
+    def displayDot(self):
+        print("({x1}, {y1}, {z1})".format(x1 = self.x, y1 = self.y, z1 = self.z))
+
+    def distanceTwoDots(self, otherDot: Dot):
+        squareX = (self.x - otherDot.x)**2
+        squareY = (self.y - otherDot.y)**2
+        squareZ = (self.z - otherDot.z)**2
+        return sqrt(squareX + squareY + squareZ)
+
+
+
+    
