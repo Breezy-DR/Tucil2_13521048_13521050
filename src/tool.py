f775dd6e80d369cced143e73a18adf3aa3618b0d
naufal <naufalfirdaus21@gmail.com>
2023-02-24 16:45:17 +0700

    main algorithm (done) so far blm ada bug

--- src/tool.py
+++ src/tool.py
@@ -34,6 +34,19 @@ def bruteforceClosest(pointList):
 
     return closestPair
 
+def minDistancePair(pair1, pair2):
+    
+    if(pair1.distance < pair2.distance):
+        return pair1
+    else:
+        return pair2
+    
+
+# hitung jarak dengan atribut x aja
+def distanceAbscissa(point1, point2):
+    
+    return abs(point1.x - point2.x)
+
 
 
 def calculateClosest(pointList):  # bidang pembatas ditengah (x, 0, 0)
@@ -68,12 +81,29 @@ def calculateClosest(pointList):  # bidang pembatas ditengah (x, 0, 0)
     else:
         middle = len(pointList) // 2
 
-        closestPair_Right = calculateClosest()
+        closestPair_Right = calculateClosest(pointList[:middle])
+        closestPair_Left = calculateClosest(pointList[middle:])
 
-    
+        closestPair = minDistancePair(closestPair_Left, closestPair_Right)
+
+        # EVALUASI BIDANG
+        # bidang yang mengandung poin dengan kemungkinan
+        # jarak lebih kecil dari closestPair
+        pointField = []
+        
+        for i in pointList:
+            if distanceAbscissa(i, pointList[middle]) < closestPair.distance:
+                pointField.append(i)
+        
+        for a in pointField:
+            for b in pointField:
+                if (a.distanceTwoDots(b) < closestPair.distance and a != b):
+                    closestPair = PairOfDots(a, b, a.distanceTwoDots(b))
+        
+        return closestPair
     
 
-points = randomSorted(3)
+points = randomSorted(100)
 
 for i in range(3):
     points[i].displayDot()
